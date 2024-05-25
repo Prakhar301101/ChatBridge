0716e82df8439afdc7f931562d9cde93411995e8
Prakhar Pandey <92363409+Prakhar301101@users.noreply.github.com>
2024-05-25 18:33:49 +0530

    Add Auth Components

--- CLIENT/src/App.jsx
+++ CLIENT/src/App.jsx
@@ -1,9 +1,9 @@
-
+import Register from "./Register";
 
 function App() {
 
   return (
-      <h1>Hello vite</h1>
+      <Register/>
   )
 }
 
---
+++ CLIENT/src/Register.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+
+
+export const Register = () => {
+    const[username,setUsername]=useState('');
+    const[password,setPassword]=useState('');
+  return (
+    <div className="bg-blue-200 h-screen flex items-center">
+        <form className="w-64 mx-auto mb-12">
+            <input
+               value={username}
+               onChange={e=>setUsername(e.target.value)}
+               type="text" placeholder="username"
+               className="block w-full rounded-sm p-2 mb-2 border"
+            />
+             <input
+             value={password}
+               onChange={e=>setPassword(e.target.value)}
+               type="password" placeholder="password"
+               className="block w-full rounded-sm p-2 mb-2 border"
+            />
+          <button className="bg-blue-500 text-white block w-full rounded-sm p-2">Register</button>
+        </form>
+    </div>
+  )
+}
